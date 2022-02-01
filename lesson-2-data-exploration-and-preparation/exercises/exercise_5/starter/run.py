6f992dbf9d316ee66d3d31aaf8761710e6623851
avpresbitero <avpresbitero@gmail.com>
2022-02-01 12:28:23 +0800

    Add solution to exercise 5

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -11,10 +11,40 @@ logger = logging.getLogger()
 
 def go(args):
 
+    # creates the wandb run
     run = wandb.init(project="exercise_5", job_type="process_data")
 
     ## YOUR CODE HERE
-    pass
+    logger.info("Fetching artifact")
+    artifact = run.use_artifact(f"{args.input_artifact}")
+    local_path = artifact.file()
+
+    logger.info("Reading artifact")
+    df = pd.read_parquet(local_path)
+
+    # Pre-processing
+    logger.info("Starting pre-processing")
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    df['title'].fillna(value="", inplace=True)
+    df['song_name'].fillna(value="", inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    output_file = args.artifact_name
+    df.to_csv(output_file)
+    # create an artifact which is just an empty shell
+    # where details are provided by the user
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description
+    )
+    # add file to the artifact to actually attach to the artifact
+    artifact.add_file(output_file)
+
+    # attach the artifact to the run
+    # non need to call finish because it automatically gets called
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
@@ -45,6 +75,8 @@ if __name__ == "__main__":
         required=True,
     )
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
+    # fix ds
+    args, unknown = parser.parse_known_args()
 
     go(args)
